fd5d6f49bbc27d8a9a895febc53cc4a6e7010f8d
Matt McNaney <mcnaneym@appstate.edu>
2021-03-15 12:51:11 -0400

    Sponsor list fleshed out.

--- javascript/Sponsor/Grid.js
+++ javascript/Sponsor/Grid.js
@@ -1,11 +1,42 @@
 'use strict'
 import React, {useState} from 'react'
 import PropTypes from 'prop-types'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faEdit} from '@fortawesome/free-solid-svg-icons'
 
-const Grid = ({listing}) => {
-  return <div>Grid</div>
+const Grid = ({listing, edit}) => {
+  const rows = listing.map((value, key) => {
+    return (
+      <tr key={`row-${value.id}`}>
+        <td style={{width: '10%'}}>
+          <button
+            className="btn btn-success btn-sm"
+            onClick={() => {
+              edit(key)
+            }}>
+            <FontAwesomeIcon icon={faEdit} />
+            &nbsp; Edit
+          </button>
+        </td>
+        <td>{value.name}</td>
+      </tr>
+    )
+  })
+  return (
+    <div>
+      <table className="table table-striped">
+        <tbody>
+          <tr>
+            <th>&nbsp;</th>
+            <th>Name</th>
+          </tr>
+          {rows}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
-Grid.propTypes = {}
+Grid.propTypes = {listing: PropTypes.array}
 
 export default Grid
