76f8c82f7f5498f6d9d97f1f4f6738cac5323df0
Matt McNaney <mcnaneym@appstate.edu>
2021-03-16 14:04:11 -0400

    Added loadCurrent() - returns currently logged in volunteer
    Added createVolunteer() - creates a volunteer from banner pull
    Added loadByUsername() - pulls student from database by username

--- class/Factory/VolunteerFactory.php
+++ class/Factory/VolunteerFactory.php
@@ -7,12 +7,59 @@
 
 namespace volunteer\Factory;
 
+use phpws2\Database;
+use volunteer\Resource\Volunteer;
+
 class VolunteerFactory extends AbstractFactory
 {
 
     public static function currentUserIsStudent()
     {
 
+    }
+
+    public static function loadCurrent()
+    {
+        $volunteer = self::loadByUsername(Authenticate::getLoginUsername());
+        //return $volunteer ?? self::createVolunteer(Authenticate::getLoginUsername());
+        if ($volunteer) {
+            return $volunteer;
+        } else {
+            return self::createVolunteer(Authenticate::getLoginUsername());
+        }
+    }
+
+    public static function loadByUsername(string $username)
+    {
+        $db = Database::getDB();
+        $tbl = $db->addTable('vol_volunteer');
+        $tbl->addFieldConditional('userName', $username);
+        $row = $db->selectOneRow();
+        if (empty($row)) {
+            return false;
+        } else {
+            $volunteer = new Volunteer;
+            $volunteer->setVars($row);
+            return $volunteer;
+        }
+    }
+
+    protected static function createVolunteer(string $username)
+    {
+        $result = Banner::pullByUsername($username);
+        if ($result['success']) {
+            $student = $result['student'];
+            $volunteer = new Volunteer;
+            $volunteer->userName = $username;
+            $volunteer->firstName = $student->firstName;
+            $volunteer->lastName = $student->lastName;
+            $volunteer->preferredName = $student->preferredName;
+            $volunteer->bannerId = (string) $student->ID;
+            self::saveResource($volunteer);
+            return $volunteer;
+        } else {
+            throw new \volunteer\Exception\StudentNotFound;
+        }
     }
 
 }
