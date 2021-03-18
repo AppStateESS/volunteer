3c4a91ad92551e40fcc861322215a851bbb9024d
Matt McNaney <mcnaneym@appstate.edu>
2021-03-18 14:31:34 -0400

    New controller for Student role sponsor data.

---
+++ class/Controller/Student/Sponsor.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace volunteer\Controller\Student;
+
+use volunteer\Controller\SubController;
+use volunteer\View\SponsorView;
+use volunteer\Factory\SponsorFactory;
+use Canopy\Request;
+
+class Sponsor extends SubController
+{
+
+    protected function listJson(Request $request)
+    {
+        return SponsorFactory::list();
+    }
+
+}
