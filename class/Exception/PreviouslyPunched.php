5546d2198240c69bf7fbc209a0e422a9dae37a5c
Matt McNaney <mcnaneym@appstate.edu>
2021-03-18 14:31:53 -0400

    Exception just for students who double punch.

---
+++ class/Exception/PreviouslyPunched.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace volunteer\Exception;
+
+class PreviouslyPunched extends Exception
+{
+
+    public function __construct()
+    {
+        $this->message = 'Student previously punched out';
+    }
+
+}
