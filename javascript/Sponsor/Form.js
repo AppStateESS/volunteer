c1d5903b3f263e96458a648d9175d58ad02496f4
Matt McNaney <mcnaneym@appstate.edu>
2021-03-22 14:14:20 -0400

    Added props

--- javascript/Sponsor/Form.js
+++ javascript/Sponsor/Form.js
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
 import {saveSponsor} from '../api/Fetch'
 
-const Form = ({sponsor, success, failure}) => {
+const Form = ({sponsor, success, failure, nameInput, reset}) => {
   const [name, setName] = useState(sponsor.name)
   useEffect(() => {
     setName(sponsor.name)
