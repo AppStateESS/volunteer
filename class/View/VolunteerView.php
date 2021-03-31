d09e964da2605b8b1ba4f3f56e397d2264f68344
Matt McNaney <mcnaneym@appstate.edu>
2021-03-31 08:49:32 -0400

    Added view for admin list of volunteers.
    Added view for a report on a volunteer's activity.
    Added sortPunches to add sponsor information.
    Login prompt moved from Module to logInPrompt.

--- class/View/VolunteerView.php
+++ class/View/VolunteerView.php
@@ -7,7 +7,55 @@
 
 namespace volunteer\View;
 
+use volunteer\Resource\Volunteer;
+use volunteer\Factory\PunchFactory;
+use volunteer\Factory\SponsorFactory;
+
 class VolunteerView extends AbstractView
 {
+
+    public static function list(array $options = [])
+    {
+        return self::scriptView('Volunteer', ['domain' => VOL_SHIB_DOMAIN]);
+    }
+
+    public static function report(Volunteer $volunteer)
+    {
+        $punches = PunchFactory::list(['volunteerId' => $volunteer->id]);
+        $vars['name'] = $volunteer->getFullName();
+        if (!empty($punches)) {
+            $vars['rows'] = self::sortPunches($punches);
+        }
+        $template = new \phpws2\Template($vars);
+        $template->setModuleTemplate('volunteer', 'Report.html');
+        return $template->get();
+    }
+
+    private static function sortPunches(array $punches)
+    {
+        $rows = [];
+        foreach ($punches as $punch) {
+            if ($punch['timeOut']) {
+                $punch['totalTime'] = PunchFactory::getTotalTime($punch['timeIn'], $punch['timeOut']);
+            } else {
+                $punch['totalTime'] = 'N/A';
+            }
+            $rows[$punch['sponsorId']]['punches'][] = $punch;
+        }
+        $sponsorIds = array_keys($rows);
+
+        $sponsorList = SponsorFactory::list(['idList' => $sponsorIds, 'sortById' => true]);
+        foreach ($sponsorList as $id => $sponsor) {
+            $rows[$id]['sponsor'] = $sponsor['name'];
+        }
+        return $rows;
+    }
+
+    public static function logInPrompt()
+    {
+        $template = new \phpws2\Template();
+        $template->setModuleTemplate('volunteer', 'Login.html');
+        return $template->get();
+    }
 
 }
