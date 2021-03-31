35df1c617a4434868be5b982f88dec519cb5678a
Matt McNaney <mcnaneym@appstate.edu>
2021-03-31 08:36:05 -0400

    Control functions added for the list page and report.

--- class/Controller/Admin/Volunteer.php
+++ class/Controller/Admin/Volunteer.php
@@ -9,10 +9,35 @@ namespace volunteer\Controller\Admin;
 
 use volunteer\Controller\SubController;
 use volunteer\View\VolunteerView;
+use volunteer\View\AdminView;
 use volunteer\Factory\VolunteerFactory;
+use volunteer\Exception\ResourceNotFound;
 use Canopy\Request;
 
 class Volunteer extends SubController
 {
+
+    protected function listHtml(Request $request)
+    {
+        AdminView::showMenu('volunteer');
+        return VolunteerView::list();
+    }
+
+    protected function listJson(Request $request)
+    {
+        return VolunteerFactory::list(VolunteerFactory::listingOptions($request));
+    }
+
+    protected function reportHtml(Request $request)
+    {
+        AdminView::showMenu('volunteer');
+        try {
+            $volunteer = VolunteerFactory::build($this->id);
+        } catch (ResourceNotFound $e) {
+            return '<p>Volunteer not found. <a href="volunteer/Admin/Volunteer/">Return to list</a></p>';
+        }
+
+        return VolunteerView::report($volunteer);
+    }
 
 }
