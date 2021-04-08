4e1050fda684a7a4af82b8880cca9570b167b249
Matt McNaney <mcnaneym@appstate.edu>
2021-04-08 12:40:09 -0400

    Added searchName variable so punch in can be done by a specific sponsor name.
    Added buildSearchName to create the searchName.

--- class/Resource/Sponsor.php
+++ class/Resource/Sponsor.php
@@ -12,12 +12,26 @@ class Sponsor extends AbstractResource
 {
 
     protected $name;
+    protected $searchName;
     protected $table = 'vol_sponsor';
 
     public function __construct()
     {
         parent::__construct();
         $this->name = new \phpws2\Variable\TextOnly(null, 'name', 50);
+        $this->searchName = new \phpws2\Variable\TextOnly(null, 'searchName', 50);
+    }
+
+    public function setName($name)
+    {
+        $this->name->set($name);
+        $this->buildSearchName();
+    }
+
+    private function buildSearchName()
+    {
+        $this->searchName->set(preg_replace('/[^\w\-]/', '',
+                        strtolower(preg_replace('/\s+/', '-', $this->name->get()))));
     }
 
 }
