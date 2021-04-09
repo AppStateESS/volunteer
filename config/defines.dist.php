2f564b7423e08f0c82d1a6f60cda5657c527a6eb
Matt McNaney <mcnaneym@appstate.edu>
2021-04-09 10:06:51 -0400

    Added VOLUNTEER_TIMEOUT

--- config/defines.dist.php
+++ config/defines.dist.php
@@ -20,6 +20,8 @@ define('VOLUNTEER_SYSTEM_SETTINGS',
 
 define('VOLUNTEER_BANNER_API', '');
 
+define('VOLUNTEER_TIMEOUT', 8);
+
 define('VOL_SHIB_DOMAIN', ''); // @university.edu
 define('VOL_SHIB_USERNAME_TAG', ''); // name of SERVER variable containing the username
 define('VOL_SHIB_LOGOUT_TAG', ''); // name of SERVER variable containing the url to log out
