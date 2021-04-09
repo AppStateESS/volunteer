d4498f3f29a712fe239afdcfd9b7dacd0f248910
Matt McNaney <mcnaneym@appstate.edu>
2021-04-09 10:01:40 -0400

    Contact email added to punch in form.

--- javascript/PunchIn/index.jsx
+++ javascript/PunchIn/index.jsx
@@ -7,22 +7,29 @@ import SponsorPunchIn from './SponsorPunchIn'
 
 /* global volunteerName, defaultSponsor */
 
-const PunchIn = ({volunteerName, defaultSponsor}) => {
+const PunchIn = ({volunteerName, defaultSponsor, contactEmail}) => {
   if (defaultSponsor) {
     return (
       <SponsorPunchIn volunteerName={volunteerName} sponsor={defaultSponsor} />
     )
   } else {
-    return <PickSponsor volunteerName={volunteerName} />
+    return (
+      <PickSponsor volunteerName={volunteerName} contactEmail={contactEmail} />
+    )
   }
 }
 
 PunchIn.propTypes = {
   volunteerName: PropTypes.string,
   defaultSponsor: PropTypes.object,
+  contactEmail: PropTypes.string,
 }
 
 ReactDOM.render(
-  <PunchIn volunteerName={volunteerName} defaultSponsor={defaultSponsor} />,
+  <PunchIn
+    volunteerName={volunteerName}
+    defaultSponsor={defaultSponsor}
+    contactEmail={contactEmail}
+  />,
   document.getElementById('PunchIn')
 )
