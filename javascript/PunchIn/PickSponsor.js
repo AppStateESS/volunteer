08affba7970b58b6d9bf14539d2f45eef2fa37cb
Matt McNaney <mcnaneym@appstate.edu>
2021-04-16 11:55:23 -0400

    Added error checks to sponsor pull.

--- javascript/PunchIn/PickSponsor.js
+++ javascript/PunchIn/PickSponsor.js
@@ -11,25 +11,34 @@ const PickSponsor = ({volunteerName, contactEmail}) => {
   const [sponsors, setSponsors] = useState([])
   const [loading, setLoading] = useState(true)
   const [sponsorId, setSponsorId] = useState(0)
+  const [error, setError] = useState(false)
+
   const loadSponsors = () => {
-    const Promise = getList('volunteer/Student/Sponsor/')
+    const Promise = getList('volunteer/Student/Sponsor')
     Promise.then((response) => {
-      setLoading(false)
-      setSponsors(response.data)
-    }).catch((error) => {
-      console.log(error)
+      if (
+        response &&
+        typeof response.data === 'object' &&
+        response.data.length > 0
+      ) {
+        setSponsors(response.data)
+      } else {
+        setSponsors([])
+        setError(true)
+      }
     })
+      .catch((error) => {
+        setSponsors([])
+        setError(true)
+      })
+      .then(() => {
+        setLoading(false)
+      })
   }
+
   let title = 'Check in'
   let sponsorList
-  if (loading) {
-    sponsorList = (
-      <div className="lead text-center">
-        <FontAwesomeIcon icon={faSpinner} spin />
-        &nbsp;Loading sponsors...
-      </div>
-    )
-  } else if (sponsors.length == 0) {
+  if (sponsors.length == 0) {
     title = 'Sorry'
     sponsorList = (
       <div className="alert alert-danger">
@@ -58,25 +67,40 @@ const PickSponsor = ({volunteerName, contactEmail}) => {
     <span>Hello {volunteerName}, please choose your sponsor and check in.</span>
   )
 
-  const content = (
-    <form method="post" action="./volunteer/Student/Punch/In">
-      {sponsorList}
-      <p className="small text-center">
-        If your sponsor is not shown,{' '}
-        <a href={`mailto:${contactEmail}`}>contact us</a>.
-      </p>
-      <button
-        type="submit"
-        className="btn btn-lg btn-success btn-block mt-3 py-3"
-        disabled={loading || sponsorId === 0 || sponsors.length == 0}>
-        {' '}
-        <FontAwesomeIcon icon={faClock} />
-        &nbsp;Check in
-      </button>
-    </form>
-  )
-
-  return <Card {...{title, subtitle, content}} />
+  if (error) {
+    return (
+      <div>
+        <h3>Sorry</h3>An error occurred when accessing our server. Please
+        contact the administrators.
+      </div>
+    )
+  } else if (loading) {
+    return (
+      <div className="lead text-center">
+        <FontAwesomeIcon icon={faSpinner} spin />
+        &nbsp;Loading sponsors...
+      </div>
+    )
+  } else {
+    const content = (
+      <form method="post" action="./volunteer/Student/Punch/In">
+        {sponsorList}
+        <p className="small text-center">
+          If your sponsor is not shown,{' '}
+          <a href={`mailto:${contactEmail}`}>contact us</a>.
+        </p>
+        <button
+          type="submit"
+          className="btn btn-lg btn-success btn-block mt-3 py-3"
+          disabled={loading || sponsorId === 0 || sponsors.length == 0}>
+          {' '}
+          <FontAwesomeIcon icon={faClock} />
+          &nbsp;Check in
+        </button>
+      </form>
+    )
+    return <Card {...{title, subtitle, content}} />
+  }
 }
 
 PickSponsor.propTypes = {
