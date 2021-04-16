57eeaec111d57eb74e775424bca4edba2e09e1c6
Matt McNaney <mcnaneym@appstate.edu>
2021-04-16 13:56:32 -0400

    Added preApproved variable.

--- class/Resource/Sponsor.php
+++ class/Resource/Sponsor.php
@@ -25,6 +25,11 @@ class Sponsor extends AbstractResource
      * @var \phpws2\Variable\Boolean
      */
     protected $kioskMode;
+
+    /**
+     * @var \phpws2\Variable\Boolean
+     */
+    protected $preApproved;
     protected $table = 'vol_sponsor';
 
     public function __construct()
@@ -33,6 +38,7 @@ class Sponsor extends AbstractResource
         $this->name = new \phpws2\Variable\TextOnly(null, 'name', 50);
         $this->searchName = new \phpws2\Variable\TextOnly(null, 'searchName', 50);
         $this->kioskMode = new \phpws2\Variable\BooleanVar(false, 'kioskMode');
+        $this->preApproved = new \phpws2\Variable\BooleanVar(false, 'preApproved');
     }
 
     public function setName($name)
