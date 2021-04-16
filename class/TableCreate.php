2805f200f3f2d8d48b4fa84f94c6d206e25fabb1
Matt McNaney <mcnaneym@appstate.edu>
2021-04-16 08:45:47 -0400

    Moved table creation and updates into new class.

---
+++ class/TableCreate.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace volunteer;
+
+use phpws2\Database;
+use phpws2\Database\ForeignKey;
+
+class TableCreate
+{
+
+    public function createEventTable()
+    {
+        $db = Database::getDB();
+        $event = new \volunteer\Resource\Event;
+        return $event->createTable($db);
+    }
+
+    public function createPunchTable()
+    {
+        $db = Database::getDB();
+        $punch = new \volunteer\Resource\Punch;
+        return $punch->createTable($db);
+    }
+
+    public function createSponsorTable()
+    {
+        $db = Database::getDB();
+        $sponsor = new \volunteer\Resource\Sponsor;
+        return $sponsor->createTable($db);
+    }
+
+    public function createVolunteerTable()
+    {
+        $db = Database::getDB();
+        $volunteer = new \volunteer\Resource\Volunteer;
+        return $volunteer->createTable($db);
+    }
+
+    public function createKioskTable()
+    {
+        $db = Database::getDB();
+        $kiosk = new \volunteer\Resource\KioskResource();
+        return $kiosk->createTable($db);
+    }
+
+    public function createKioskColumn()
+    {
+        $db = Database::getDB();
+        $sponsor = $db->addTable('vol_sponsor');
+        $km = $sponsor->addDataType('kioskMode', 'smallint');
+        $km->add();
+    }
+
+}
