61cadfc41271bf0fa3bbd15eb057b25266c1b1ba
Matt McNaney <mcnaneym@appstate.edu>
2021-04-21 13:57:53 -0400

    Added function to send approves (incomplete)

--- javascript/VolunteerReport/Grid.js
+++ javascript/VolunteerReport/Grid.js
@@ -1,8 +1,8 @@
 'use strict'
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import dayjs from 'dayjs'
-import {ajaxPunchOut} from '../api/Fetch'
+import {ajaxPunchOut, sendApproves} from '../api/Fetch'
 
 const sendPunchOut = (punchId, load) => {
   const promise = ajaxPunchOut(punchId)
@@ -20,6 +20,7 @@ const Approved = React.memo(({approved}) => {
     <div className="badge badge-danger">Not approved</div>
   )
 })
+Approved.propTypes = {approved: PropTypes.number}
 
 const punchOut = (punch, load) => {
   if (punch.timeOut > 0) {
@@ -35,9 +36,19 @@ const punchOut = (punch, load) => {
   }
 }
 
-const Punch = ({punch, load}) => {
+const Punch = ({punch, load, approve}) => {
   return (
     <tr>
+      <td>
+        {punch.approved == 0 ? (
+          <input
+            type="checkbox"
+            name="approve[]"
+            value={punch.id}
+            onChange={() => approve(punch.id)}
+          />
+        ) : null}
+      </td>
       <td>{dayjs(punch.timeIn * 1000).format('MMM. D, YYYY')}</td>
       <td>{dayjs(punch.timeIn * 1000).format('h:mm A')}</td>
       <td>{punchOut(punch, load)}</td>
@@ -52,21 +63,41 @@ const Punch = ({punch, load}) => {
 Punch.propTypes = {punch: PropTypes.object, load: PropTypes.func}
 
 const Grid = ({listing, load}) => {
+  const [approveList, setApproveList] = useState({})
+  const approve = (punchId) => {
+    const copyList = Object.assign({}, approveList)
+    if (copyList[punchId]) {
+      delete copyList[punchId]
+    } else {
+      copyList[punchId] = true
+    }
+    setApproveList(copyList)
+  }
+
   const rows = listing.map((value, key) => {
     const punches = value.punches.map((value) => {
-      return <Punch key={`punch-${value.id}`} punch={value} load={load} />
+      return (
+        <Punch
+          key={`punch-${value.id}`}
+          punch={value}
+          load={load}
+          approve={approve}
+        />
+      )
     })
     return (
       <div key={`sponsor-group-${key}`}>
         <h3>{value.sponsor}</h3>
+
         <table className="table table-striped">
           <tbody>
             <tr>
+              <th style={{width: '5%'}}></th>
               <th style={{width: '20%'}}>Day</th>
               <th style={{width: '20%'}}>Arrived</th>
               <th style={{width: '20%'}}>Left</th>
               <th style={{width: '20%'}}>Total time</th>
-              <th style={{width: '20%'}}>Status</th>
+              <th style={{width: '15%'}}>Status</th>
             </tr>
             {punches}
           </tbody>
@@ -74,7 +105,18 @@ const Grid = ({listing, load}) => {
       </div>
     )
   })
-  return <div>{rows}</div>
+  return (
+    <div>
+      <button
+        className="btn btn-outline-dark"
+        onClick={() => {
+          sendApproves(Object.keys(approveList))
+        }}>
+        Approved checked
+      </button>
+      {rows}
+    </div>
+  )
 }
 
 Grid.propTypes = {listing: PropTypes.array, load: PropTypes.func}
