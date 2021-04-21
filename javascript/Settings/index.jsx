c548db5619a6d9853c4b854d00a9592293a12463
Matt McNaney <mcnaneym@appstate.edu>
2021-04-21 13:56:30 -0400

    Added setting for approval required on punches.

--- javascript/Settings/index.jsx
+++ javascript/Settings/index.jsx
@@ -1,20 +1,45 @@
 'use strict'
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
+import BigCheckBox from '@essappstate/canopy-react-bigcheckbox'
+import {updateSetting} from '../api/Fetch'
+
 /* global currentSettings */
 const Settings = ({currentSettings}) => {
   const [settings, setSettings] = useState(currentSettings)
+
+  const setValue = (name, value) => {
+    const copy = Object.assign({}, settings)
+    copy[name] = value
+    setSettings(copy)
+    updateSetting(name, value)
+  }
+
+  const setApprovalRequired = () => {
+    setValue('approvalRequired', !settings.approvalRequired)
+  }
+
   return (
     <div>
       <h2>Settings</h2>
-      <table className="table">
-        <tbody>
-          <tr>
-            <th>Run sponsor in kiosk mode</th>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="row">
+        <div className="col-md-8 col-sm-10 mx-auto">
+          <table className="table">
+            <tbody>
+              <tr>
+                <th>Punch approval is required</th>
+                <td>
+                  <BigCheckBox
+                    label="Approval required"
+                    checked={settings.approvalRequired}
+                    handle={setApprovalRequired}
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   )
 }
