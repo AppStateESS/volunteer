ad7733387639361ae236bf26443c2fb319fb9579
Matt McNaney <mcnaneym@appstate.edu>
2021-04-22 12:59:15 -0400

    Moved into own file for sharing.
    Fixed #2 Too many minutes.

---
+++ javascript/api/Time.js
@@ -0,0 +1,18 @@
+'use strict'
+import dayjs from 'dayjs'
+const totalTime = (punch) => {
+  const inTime = punch.timeIn * 1000
+  const outTime = punch.timeOut * 1000
+  const date1 = dayjs(inTime)
+  if (outTime > 0) {
+    const date2 = dayjs(outTime)
+    const hours = date2.diff(date1, 'hour')
+    const minutes = Math.floor(date2.diff(date1, 'minute') % 60)
+
+    return `${hours} hour(s), ${minutes} minute(s)`
+  } else {
+    return 'N/A'
+  }
+}
+
+export default totalTime
