485b9ee5cc03d78e2b40a1cbe2f47116e52cc659
Matt McNaney <mcnaneym@appstate.edu>
2021-05-04 12:02:58 -0400

    Added missing kiosk table drop.

--- boost/uninstall.php
+++ boost/uninstall.php
@@ -7,6 +7,7 @@ function volunteer_uninstall(&$content)
     $db->buildTable('vol_event')->drop(true);
     $db->buildTable('vol_volunteer')->drop(true);
     $db->buildTable('vol_punch')->drop(true);
+    $db->buildTable('vol_kiosk')->drop(true);
 
     // From previous version
     return true;
