82e46ae43f3c8f8cf0fcd6338de0116a81b90b51
Matt McNaney <mcnaneym@appstate.edu>
2021-05-07 13:20:37 -0400

    Changed report button to report and log dropdown.

--- javascript/Volunteer/Grid.js
+++ javascript/Volunteer/Grid.js
@@ -1,21 +1,38 @@
 'use strict'
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import FullName from '../api/Name'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faList} from '@fortawesome/free-solid-svg-icons'
 
 const Grid = ({listing, domain}) => {
+  const [selected, setSelected] = useState('na')
+
+  const adminOption = (option, id) => {
+    switch (option) {
+      case 'report':
+        location.href = `./volunteer/Admin/Volunteer/${id}/report`
+        break
+      case 'log':
+        location.href = `./volunteer/Admin/Log/?volunteerId=${id}`
+        break
+    }
+  }
+
   const rows = listing.map((value) => {
     return (
       <tr key={`row-${value.id}`}>
         <td style={{width: '15%'}}>
-          <a
-            className="btn btn-outline-dark btn-sm"
-            href={`./volunteer/Admin/Volunteer/${value.id}/report`}>
-            <FontAwesomeIcon icon={faList} />
-            &nbsp;Report
-          </a>
+          <select
+            onChange={(e) => adminOption(e.target.value, value.id)}
+            value={selected}
+            className="form-control-sm">
+            <option disabled={true} value="na" className="text-center">
+              - Commands -
+            </option>
+            <option value="report">Report</option>
+            <option value="log">Log</option>
+          </select>
         </td>
         <td>
           <FullName volunteer={value} useAbbr={false} />
