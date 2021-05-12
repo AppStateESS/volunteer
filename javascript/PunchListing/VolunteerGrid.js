c553725e5fa2fb03e5d87212feccc7f20550a7c8
Matt McNaney <mcnaneym@appstate.edu>
2021-05-12 12:48:20 -0400

    Finished out volunteer report.

--- javascript/PunchListing/VolunteerGrid.js
+++ javascript/PunchListing/VolunteerGrid.js
@@ -1,11 +1,100 @@
 'use strict'
-import React, {useState} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
+import {Day, TimeFormat, ApproveButton, TimeOut, ChangeTime} from './Time'
+import FullName from '../api/Name'
 
-const VolunteerGrid = ({}) => {
-  return <div>VolunteerGrid</div>
+const VolunteerGrid = ({volunteer, listing, punchOut, approve, edit}) => {
+  let rows
+  const totalTime = (value) => {
+    return (
+      <tr className="bg-primary text-white">
+        <td colSpan="6">
+          <strong>Total time with {value.sponsor}</strong>
+          &nbsp;&ndash;&nbsp;{value.totalTime}
+        </td>
+      </tr>
+    )
+  }
+  if (listing.length == 0) {
+    rows = (
+      <tr>
+        <td colSpan="6">No rows found.</td>
+      </tr>
+    )
+  } else {
+    rows = listing.map((value, skey) => {
+      const punches = value.punches.map((value, pkey) => {
+        return (
+          <tr key={`row-${value.id}`}>
+            <td>
+              {value.timeOut ? (
+                <ChangeTime edit={() => edit(pkey)} />
+              ) : (
+                <span></span>
+              )}
+            </td>
+            <td>
+              <Day time={value.timeIn} />
+            </td>
+            <td>
+              <TimeFormat time={value.timeIn} />
+            </td>
+            <td>
+              <TimeOut
+                punch={value}
+                punchOut={punchOut}
+                edit={() => edit(pkey)}
+              />
+            </td>
+            <td>{value.totalTime}</td>
+            <td>
+              <ApproveButton value={value} approve={approve} />
+            </td>
+          </tr>
+        )
+      })
+      return (
+        <div key={`sponsor-group-${skey}`}>
+          <h3>{value.sponsor}</h3>
+          <table className="table table-striped">
+            <tbody>
+              <tr>
+                <th style={{width: '5%'}}></th>
+                <th style={{width: '20%'}}>Day</th>
+                <th style={{width: '20%'}}>Clock in</th>
+                <th style={{width: '20%'}}>Clock out</th>
+                <th style={{width: '25%'}}>Total time</th>
+                <th style={{width: '10%'}}>Approved</th>
+              </tr>
+              {punches}
+              {totalTime(value)}
+            </tbody>
+          </table>
+          <hr className="my-4" />
+        </div>
+      )
+    })
+  }
+
+  return (
+    <div>
+      <h2>
+        Punches for volunteer:{' '}
+        <FullName volunteer={volunteer} useAbbr={false} />
+      </h2>
+
+      {rows}
+    </div>
+  )
 }
 
-VolunteerGrid.propTypes = {}
+VolunteerGrid.propTypes = {
+  volunteer: PropTypes.object,
+  listing: PropTypes.array,
+  punchOut: PropTypes.func,
+  approve: PropTypes.func,
+  edit: PropTypes.func,
+}
 
 export default VolunteerGrid
