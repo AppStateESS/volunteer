a293a1c1f9e33fbcd7f3f5fc5b348a18ee0199c9
Matt McNaney <mcnaneym@appstate.edu>
2021-06-30 09:51:40 -0400

    Added attendanceOnly column.

--- class/Resource/Sponsor.php
+++ class/Resource/Sponsor.php
@@ -30,6 +30,11 @@ class Sponsor extends AbstractResource
      * @var \phpws2\Variable\Boolean
      */
     protected $preApproved;
+
+    /**
+     * @var \phpws2\Variable\Boolean
+     */
+    protected $attendanceOnly;
     protected $table = 'vol_sponsor';
 
     public function __construct()
@@ -39,6 +44,7 @@ class Sponsor extends AbstractResource
         $this->searchName = new \phpws2\Variable\TextOnly(null, 'searchName', 50);
         $this->kioskMode = new \phpws2\Variable\BooleanVar(false, 'kioskMode');
         $this->preApproved = new \phpws2\Variable\BooleanVar(false, 'preApproved');
+        $this->attendanceOnly = new \phpws2\Variable\BooleanVar(false, 'attendanceOnly');
     }
 
     public function setName($name)
