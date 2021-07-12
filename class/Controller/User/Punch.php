6cf03ae6f2df33b1a5f011c2b6950e95e4331ad0
Matt McNaney <mcnaneym@appstate.edu>
2021-07-12 12:59:47 -0400

    Added clockInReasonPost - control for kiosk swipe in with reason.
    swipeInJson now returns reasons array and the volunteerId if the sponsor
    has reason selection.

--- class/Controller/User/Punch.php
+++ class/Controller/User/Punch.php
@@ -11,6 +11,8 @@ use volunteer\Controller\SubController;
 use volunteer\View\KioskView;
 use volunteer\Factory\VolunteerFactory;
 use volunteer\Factory\PunchFactory;
+use volunteer\Factory\SponsorFactory;
+use volunteer\Factory\ReasonFactory;
 use Canopy\Request;
 
 class Punch extends SubController
@@ -21,10 +23,22 @@ class Punch extends SubController
         return KioskView::scriptView('Kiosk', ['sponsor' => $GLOBALS['currentSponsor']]);
     }
 
+    protected function clockInReasonPost(Request $request)
+    {
+        $sponsorId = $request->pullPostInteger('sponsorId');
+        $volunteerId = $request->pullPOstInteger('volunteerId');
+        $reasonId = $request->pullPostInteger('reasonId');
+        $volunteer = VolunteerFactory::build($volunteerId);
+        $reason = ReasonFactory::build($reasonId);
+        PunchFactory::in($volunteer, $sponsorId, $reasonId);
+        return ['success' => true, 'attendanceOnly' => $reason->forceAttended];
+    }
+
     protected function swipeInJson(Request $request)
     {
         $studentBannerId = $request->pullGetString('studentBannerId');
         $sponsorId = $request->pullGetString('sponsorId');
+        $sponsor = SponsorFactory::build($sponsorId);
         $volunteer = VolunteerFactory::loadByBannerId($studentBannerId);
         if (!$volunteer) {
             try {
@@ -44,8 +58,19 @@ class Punch extends SubController
                 return ['success' => true, 'result' => 'out'];
             }
         } else {
-            PunchFactory::in($volunteer, $sponsorId);
-            return ['success' => true, 'result' => 'in'];
+            if ($sponsor->useReasons) {
+                /* Error check to prevent blank reason page */
+                $reasons = ReasonFactory::listing(['sponsorId' => $sponsor->id]);
+                if (empty($reasons)) {
+                    PunchFactory::in($volunteer, $sponsorId);
+                    return ['success' => true, 'result' => 'in', 'reasons' => []];
+                } else {
+                    return ['success' => true, 'result' => 'reason', 'reasons' => $reasons, 'volunteerId' => $volunteer->id];
+                }
+            } else {
+                PunchFactory::in($volunteer, $sponsorId);
+                return ['success' => true, 'result' => 'in', 'reasons' => []];
+            }
         }
     }
 
