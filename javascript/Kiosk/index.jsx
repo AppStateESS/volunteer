92f753360d015f3136efd84a3b4a7ca18359d9be
Matt McNaney <mcnaneym@appstate.edu>
2021-08-04 12:33:01 -0400

    For issue #21, added method for non-students to log in.
    - The code also allows students to log in at kiosk without banner id.
    - Added new modal information, email checks, and code for visitor
      handling.
    - Changed wording upon clock in and out.
    - Response from server on punch is now in determineResponse function.

--- javascript/Kiosk/index.jsx
+++ javascript/Kiosk/index.jsx
@@ -4,91 +4,168 @@ import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
 import Card from '../api/Card'
 import ClockInput from './ClockInput'
+import NewVisitor from './NewVisitor'
 import ReasonList from '../api/ReasonList'
 import Overlay from '@essappstate/canopy-react-overlay'
-import {swipeVolunteer, clockInReason} from '../api/Fetch'
+import {
+  swipeVolunteer,
+  clockInReason,
+  getEmail,
+  saveNewVisitor,
+} from '../api/Fetch'
+
+const emailFormatCorrect = (email) => {
+  const expression = /[\S]+@\S+\.\S{2,3}/
+  return expression.test(String(email).toLowerCase())
+}
 
 /* global sponsor */
 const Kiosk = ({sponsor}) => {
   const [error, setError] = useState('')
+  const [reasonsLoaded, setReasonsLoaded] = useState(false)
   const [message, setMessage] = useState('')
   const [reasons, setReasons] = useState([])
   const [reasonId, setReasonId] = useState(0)
   const [lockInput, setLockInput] = useState(false)
-  const [showModal, setShowModal] = useState(false)
+  const [showReasonModal, setShowReasonModal] = useState(false)
+  const [showVisitorModal, setShowVisitorModal] = useState(false)
   const [volunteerId, setVolunteerId] = useState(0)
-  const [attended, setAttended] = useState(false)
+  const [email, setEmail] = useState('')
+  const [goodEmail, setGoodEmail] = useState(0)
 
   useEffect(() => {
     if (reasonId > 0) {
       clockInReason(sponsor.id, volunteerId, reasonId).then((response) => {
         if (response.data.attendanceOnly) {
           setMessage(
-            'Thank you for being here today. You do not need to swipe out.'
+            'Thank you for being here today. You do not need to clock out.'
           )
         } else {
-          setMessage(
-            'Thank you for clocking in. Please swipe again when you leave.'
-          )
+          setMessage('Thank you. Please clock out when you leave.')
         }
-        setShowModal(false)
+        setShowReasonModal(false)
         pauseAndReset()
       })
     }
   }, [reasonId])
 
+  useEffect(() => {
+    if (email.length > 0) {
+      setGoodEmail(emailFormatCorrect(email) ? 1 : -1)
+    }
+  }, [email])
+
+  const resetVisitor = () => {
+    setEmail('')
+    setGoodEmail(0)
+    setShowVisitorModal(false)
+  }
+
+  const saveVisitor = (firstName, lastName) => {
+    resetVisitor()
+    setLockInput(true)
+    const values = {
+      firstName,
+      lastName,
+      email,
+      sponsorId: sponsor.id,
+      includeReasons: !reasonsLoaded,
+    }
+    saveNewVisitor(values).then((response) => {
+      determineResponse(response)
+      setLockInput(false)
+    })
+  }
+
   const pauseAndReset = () => {
     const reset = setTimeout(() => {
-      setAttended(false)
       setVolunteerId(0)
       setReasonId(0)
+      setEmail('')
+      setGoodEmail(0)
       setError('')
       setMessage('')
+      setShowVisitorModal(false)
+      setShowReasonModal(false)
       clearTimeout(reset)
     }, 4000)
   }
 
+  const checkEmail = (e) => {
+    const address = e.target.value
+    if (!address.match(/[<>]/)) {
+      setEmail(address)
+    }
+  }
+
+  const sendEmail = (email) => {
+    getEmail({
+      email,
+      sponsorId: sponsor.id,
+      includeReasons: !reasonsLoaded,
+    }).then((r) => {
+      if (r.data.success) {
+        resetVisitor()
+        determineResponse(r)
+      } else {
+        setShowVisitorModal(true)
+      }
+    })
+  }
+
+  const determineResponse = (response) => {
+    const {success, result} = response.data
+    console.log(success)
+    if (success) {
+      switch (result) {
+        case 'in':
+          if (sponsor.attendanceOnly == 1) {
+            setMessage(
+              'Thank you for being with us today. You do not need to clock out.'
+            )
+          } else {
+            setMessage(
+              'Thank you for being with us today. Remember to clock out when you leave.'
+            )
+          }
+          pauseAndReset()
+          break
+        case 'out':
+          setMessage('Thank you, you have clocked out.')
+          pauseAndReset()
+          break
+        case 'reason':
+          if (
+            reasonsLoaded === false &&
+            response.data.reasons &&
+            response.data.reasons.length
+          ) {
+            setReasonsLoaded(true)
+            setReasons(response.data.reasons)
+          }
+          setVolunteerId(response.data.volunteerId)
+          setShowReasonModal(true)
+          break
+      }
+    } else {
+      switch (result) {
+        case 'punchedInElsewhere':
+          setError('You are currently clocked in elsewhere.')
+          pauseAndReset()
+          break
+        case 'notfound':
+          setError('Could not find your account. Check at the desk.')
+          pauseAndReset()
+          break
+      }
+    }
+  }
+
   const sendSwipe = (studentBannerId) => {
     setLockInput(true)
-    const Promise = swipeVolunteer(studentBannerId, sponsor.id)
+    const Promise = swipeVolunteer(studentBannerId, sponsor.id, !reasonsLoaded)
     Promise.then((response) => {
-      const {success, result} = response.data
-      if (success) {
-        switch (result) {
-          case 'in':
-            if (sponsor.attendanceOnly == 1) {
-              setMessage(
-                'Thank you for being here today. You do not need to swipe out.'
-              )
-            } else {
-              setMessage(
-                'Thank you for clocking in. Remember to swipe out when you leave.'
-              )
-            }
-            pauseAndReset()
-            break
-          case 'out':
-            setMessage('Thank you, you have clocked out.')
-            pauseAndReset()
-            break
-          case 'reason':
-            setReasons(response.data.reasons)
-            setVolunteerId(response.data.volunteerId)
-            setShowModal(true)
-            break
-        }
-      } else {
-        switch (result) {
-          case 'punchedInElsewhere':
-            setError('You are currently clocked in elsewhere.')
-            pauseAndReset()
-            break
-          case 'notfound':
-            setError('Could not find your account. Check at the desk.')
-            pauseAndReset()
-            break
-        }
-      }
+      determineResponse(response)
       setLockInput(false)
     })
   }
@@ -98,11 +175,43 @@ const Kiosk = ({sponsor}) => {
   } else if (message.length > 0) {
     return <div className="alert alert-success lead text-center">{message}</div>
   } else {
+    let message
+    if (goodEmail === -1) {
+      message = <div className="text-danger small">Check email format</div>
+    } else if (goodEmail === 1) {
+      message = <div className="text-success small">Email looks good!</div>
+    }
+    const visitorInput = (
+      <div>
+        <div className="input-group mb-3">
+          <input
+            autoComplete="off"
+            type="text"
+            className="form-control"
+            name="email"
+            value={email}
+            onChange={checkEmail}
+            disabled={lockInput}
+          />
+          <div className="input-group-append">
+            <button
+              className="btn btn-success"
+              disabled={goodEmail !== 1}
+              onClick={() => {
+                sendEmail(email)
+              }}>
+              Go!
+            </button>
+          </div>
+        </div>
+        {message}
+      </div>
+    )
     return (
       <div>
         <Overlay
-          show={showModal}
-          close={() => setShowModal(false)}
+          show={showReasonModal}
+          close={() => setShowReasonModal(false)}
           title="Reason for visit"
           width="80%">
           <ReasonList
@@ -112,7 +221,15 @@ const Kiosk = ({sponsor}) => {
             }}
           />
         </Overlay>
+        <Overlay
+          show={showVisitorModal}
+          close={() => setShowVisitorModal(false)}
+          title="Create your account"
+          width="80%">
+          <NewVisitor saveVisitor={saveVisitor} email={email} />
+        </Overlay>
         <Card
+          cn="mb-4"
           title={`Welcome to ${sponsor.name}`}
           subtitle={
             <div className="text-center">
@@ -122,6 +239,16 @@ const Kiosk = ({sponsor}) => {
             </div>
           }
           content={<ClockInput sendSwipe={sendSwipe} lockInput={lockInput} />}
+        />
+        <Card
+          headerColor="success"
+          title="Not a student or forgot your Banner ID?"
+          subtitle={
+            <div className="text-center">
+              Please enter your email address below.
+            </div>
+          }
+          content={visitorInput}
         />
       </div>
     )
