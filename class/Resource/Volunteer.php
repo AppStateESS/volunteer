1230964a923d1dedb532342ac633a85f0e3d9440
Matt McNaney <mcnaneym@appstate.edu>
2021-08-04 12:29:39 -0400

    Added an email, lastLog, and totalVisit column.
    Added stampVisit function to clock when last punched out and their
    total visits.

--- class/Resource/Volunteer.php
+++ class/Resource/Volunteer.php
@@ -11,21 +11,30 @@ class Volunteer extends AbstractResource
 {
 
     protected $userName;
+    protected $email;
     protected $bannerId;
     protected $firstName;
     protected $preferredName;
     protected $lastName;
+    protected $lastLog;
+    protected $totalVisits;
     protected $table = 'vol_volunteer';
 
     public function __construct()
     {
         parent::__construct();
         $this->userName = new \phpws2\Variable\StringVar(null, 'userName', 50);
-        $this->bannerId = new \phpws2\Variable\NumberString(null, 'bannerId', 20);
+        $this->userName->allowNull(true);
+        $this->email = new \phpws2\Variable\Email(null, 'email', 100);
+        $this->bannerId = new \phpws2\Variable\NumberString('0', 'bannerId', 20);
+        $this->bannerId->allowNull(true);
         $this->firstName = new \phpws2\Variable\TextOnly(null, 'firstName', 40);
         $this->preferredName = new \phpws2\Variable\TextOnly(null, 'preferredName', 40);
         $this->preferredName->allowNull(true);
         $this->lastName = new \phpws2\Variable\TextOnly(null, 'lastName', 40);
+        $this->totalVisits = new \phpws2\Variable\IntegerVar(0, 'totalVisits');
+        $this->lastLog = new \phpws2\Variable\DateTime(0, 'lastLog');
+        $this->lastLog->setFormat('%c');
     }
 
     public function getFullName()
@@ -40,6 +49,12 @@ class Volunteer extends AbstractResource
     public function getPreferred()
     {
         return $this->preferredName->isEmpty() ? $this->firstName->get() : $this->preferredName->get();
+    }
+
+    public function stampVisit()
+    {
+        $this->lastLog->stamp();
+        $this->totalVisits->increase();
     }
 
 }
