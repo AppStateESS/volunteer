82825b6c12ecf791017c5e401b164bd846ca4e26
Matt McNaney <mcnaneym@appstate.edu>
2021-08-04 12:27:42 -0400

    createVolunteer call changed to createStudent.
    - Added pullFromBannerByEmail function. Uses an email address to get
      Banner data.
    - Added buildVolunteerFromBannerResult(). Creates a volunteer object
      from the Banner result data.
    - Added stampVisit function to record last logged and visit information.
    - Added getByEmail. Returns a CURRENT volunteer by their email address.
    - Moved volunteer creation out of createStudent and into
      buildVolunteerFromBannerResult.
    - Added createVisitor function to build a volunteer from email, first
    and last name. Resolves #21.
    - With addition of lastLog and totalVisits, added formatted date for
      lastLog.

--- class/Factory/VolunteerFactory.php
+++ class/Factory/VolunteerFactory.php
@@ -43,7 +43,7 @@ class VolunteerFactory extends AbstractFactory
         if ($volunteer) {
             return $volunteer;
         } elseif ($createIfNotFound) {
-            return self::createVolunteer(Authenticate::getLoginUsername());
+            return self::createStudent(Authenticate::getLoginUsername());
         } else {
             Authenticate::sendToLogin();
         }
@@ -79,29 +79,130 @@ class VolunteerFactory extends AbstractFactory
         }
     }
 
-    public static function createVolunteer(string $identifier)
+    /**
+     * Tries to pull a student from Banner using the email address.
+     * If it exists, then a volunteer account is made and returned.
+     * This function assumes vol_volunteer was checked prior.
+     * @param string $email
+     * @return Volunteer
+     */
+    public static function pullFromBannerByEmail(string $email)
+    {
+        // If the email address doesn't match our expected domain
+        // we don't bother continuing
+        $searchString = '/' . VOL_SHIB_DOMAIN . '$/';
+        if (!preg_match($searchString, $email)) {
+            return null;
+        }
+        $username = preg_replace($searchString, '', $email);
+        $result = Banner::queryServer($username);
+        if ($result) {
+            return self::buildVolunteerFromBannerResult($result);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Expects an Banner object result and creates a new Volunteer
+     * from the data.
+     * @param object $result
+     * @return Volunteer
+     */
+    private static function buildVolunteerFromBannerResult(array $result)
+    {
+        $student = $result['student'];
+        $volunteer = new Volunteer;
+        $volunteer->userName = $student->userName;
+        $volunteer->email = $student->userName . VOL_SHIB_DOMAIN;
+        $volunteer->firstName = $student->firstName;
+        $volunteer->lastName = $student->lastName;
+        $volunteer->preferredName = $student->preferredName ?? null;
+        $volunteer->bannerId = (string) $student->bannerID;
+        self::saveResource($volunteer);
+        return $volunteer;
+    }
+
+    public static function stampVisit(int $volunteerId)
+    {
+        $volunteer = self::build($volunteerId);
+        if ($volunteer) {
+            $volunteer->stampVisit();
+            self::save($volunteer);
+        }
+    }
+
+    /**
+     * Pulls a single visitor from the database.
+     * @param string $email
+     * @return Volunteer
+     */
+    public static function getByEmail(string $email)
+    {
+        $db = Database::getDB();
+        $tbl = $db->addTable('vol_volunteer');
+        $tbl->addFieldConditional('email', $email);
+        $row = $db->selectOneRow();
+        if (empty($row)) {
+            return;
+        }
+        $volunteer = self::build();
+        $volunteer->setVars($row);
+        return $volunteer;
+    }
+
+    /**
+     * Creates a volunteer based on the received Banner ID.
+     * @param string $identifier
+     * @return Volunteer
+     * @throws \volunteer\Exception\StudentNotFound
+     */
+    public static function createStudent(string $identifier)
     {
         $result = Banner::queryServer($identifier);
 
         if ($result['success']) {
-            $student = $result['student'];
-            $volunteer = new Volunteer;
-            $volunteer->userName = $student->userName;
-            $volunteer->firstName = $student->firstName;
-            $volunteer->lastName = $student->lastName;
-            $volunteer->preferredName = $student->preferredName ?? null;
-            $volunteer->bannerId = (string) $student->bannerID;
-            self::saveResource($volunteer);
-            return $volunteer;
+            return self::buildVolunteerFromBannerResult($result);
         } else {
             throw new \volunteer\Exception\StudentNotFound($identifier);
         }
+    }
+
+    /**
+     * Creates a non-student (no Banner ID) volunteer.
+     * @param string $email
+     * @param string $firstName
+     * @param string $lastName
+     * @return Volunteer
+     */
+    public static function createVisitor(string $email, string $firstName, string $lastName)
+    {
+        $volunteer = new Volunteer;
+        $volunteer->userName = $email;
+        $volunteer->email = $email;
+        $volunteer->firstName = $firstName;
+        $volunteer->lastName = $lastName;
+        $volunteer->preferredName = null;
+        $volunteer->bannerId = '';
+        self::saveResource($volunteer);
+        return $volunteer;
     }
 
     public static function list($options = [])
     {
         $db = Database::getDB();
         $tbl = $db->addTable('vol_volunteer');
+        $tbl->addField('userName');
+        $tbl->addField('bannerId');
+        $tbl->addField('firstName');
+        $tbl->addField('preferredName');
+        $tbl->addField('lastName');
+        $tbl->addField('id');
+        $tbl->addField('email');
+        $tbl->addField('totalVisits');
+        $lastLog = $tbl->getField('lastLog');
+        $exp = new Database\Expression('FROM_UNIXTIME(' . $lastLog . ', "%Y/%m/%d %e %l:%i %p")', 'lastLog');
+        $tbl->addField($exp);
         parent::applyOptions($db, $tbl, $options, ['firstName', 'lastName', 'preferredName']);
         return $db->select();
     }
