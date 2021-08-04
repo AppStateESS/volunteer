864fc0fa73390c18657163b1e4f1af2128e4eaed
Matt McNaney <mcnaneym@appstate.edu>
2021-08-04 12:33:58 -0400

    Changed to all cn prop to control header color.

--- javascript/api/Card.js
+++ javascript/api/Card.js
@@ -2,13 +2,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Card = ({title, subtitle, content}) => {
+const Card = ({title, subtitle, content, cn = '', headerColor = 'primary'}) => {
   return (
-    <div className="container">
+    <div className={`container ${cn}`}>
       <div className="row d-flex justify-content-center">
         <div className="col-md-8 col-sm-10">
           <div className="card">
-            <div className="card-header bg-primary text-white">
+            <div className={`card-header bg-${headerColor} text-white`}>
               <h2 className="m-0">{title}</h2>
             </div>
             <div className="card-body">
@@ -26,6 +26,7 @@ Card.propTypes = {
   title: PropTypes.PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   subtitle: PropTypes.PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   content: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  cn: PropTypes.string,
 }
 
 export default Card
