8d4d0ff40762846facf4a954715260da4be438a9
Matt McNaney <mcnaneym@appstate.edu>
2022-02-14 12:18:28 -0500

    Added missing VOL_SHIB_LOGOUT_TAG

--- config/defines.dist.php
+++ config/defines.dist.php
@@ -13,15 +13,16 @@
  * Do not change the below unless developing
  */
 define('VOLUNTEER_SYSTEM_SETTINGS',
-        [
-            'productionMode' => true,
-            'friendlyErrors' => true
-]);
+    [
+        'productionMode' => true,
+        'friendlyErrors' => true
+    ]);
 
 define('VOLUNTEER_BANNER_API', '');
 
 define('VOLUNTEER_TIMEOUT', 8);
 
 define('VOL_SHIB_DOMAIN', ''); // @university.edu
+define('VOL_SHIB_LOGOUT_TAG', '');
 define('VOL_SHIB_USERNAME_TAG', ''); // name of SERVER variable containing the username
 define('VOL_CONTACT_EMAIL', '');
