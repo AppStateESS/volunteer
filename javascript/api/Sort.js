06a6477b60155689aaaf1777f349cdef3b0031ce
Matt McNaney <mcnaneym@appstate.edu>
2022-02-15 13:50:44 -0500

    Added num as a different type of button.

--- javascript/api/Sort.js
+++ javascript/api/Sort.js
@@ -3,14 +3,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
-const Sort = ({direction, handleClick, label}) => {
+const Sort = ({direction, handleClick, label, type}) => {
   if (direction === undefined || direction === null) {
     direction = 'none'
   }
+
   const sortIcons = {
     asc: 'sort-alpha-up',
     desc: 'sort-alpha-down-alt',
     none: 'sort',
+  }
+  switch (type) {
+    case 'num':
+      sortIcons.asc = 'sort-numeric-up'
+      sortIcons.desc = 'sort-numeric-down-alt'
   }
 
   return (
@@ -24,5 +30,6 @@ Sort.propTypes = {
   direction: PropTypes.string,
   handleClick: PropTypes.func,
   label: PropTypes.string,
+  type: PropTypes.string,
 }
 export default Sort
