52539e779a719f065415f85e79c78d8bbf660653
Matt McNaney <mcnaneym@appstate.edu>
2022-03-02 10:52:50 -0500

    Added qrCode view.
    Creates an image using chillerlan\QRCode Composer library.

--- class/View/SponsorView.php
+++ class/View/SponsorView.php
@@ -7,7 +7,29 @@
 
 namespace volunteer\View;
 
+use volunteer\Factory\SponsorFactory;
+use phpws2\Template;
+
+require_once PHPWS_SOURCE_DIR . 'mod/volunteer/vendor/autoload.php';
+
+use chillerlan\QRCode\QRCode;
+use chillerlan\QRCode\QROptions;
+
 class SponsorView extends AbstractView
 {
-//put your code here
+
+    public static function qrCode($sponsorId)
+    {
+        $sponsor = SponsorFactory::build($sponsorId);
+        $url = SponsorFactory::getUrl($sponsor);
+        $options = new QROptions(['scale' => 10]);
+        $qrcode = new QRCode($options);
+
+        $vars['image'] = '<img style="width: 800px;height: 800px" src="' . $qrcode->render($url) . '"/>';
+        $vars['sponsorName'] = $sponsor->name;
+        $template = new Template($vars);
+        $template->setModuleTemplate('volunteer', 'QRCode.html');
+        return $template->get();
+    }
+
 }
