47eb8af6fa2fe0f581a99ac0029a146f035cbd0e
Matt McNaney <mcnaneym@appstate.edu>
2022-03-16 11:25:26 -0400

    Adds new controls for quick login.
    * Adds afterHtml - shown after quick clocking in or out.
    * Adds quickPost - accepts a quick clock in or out of a sponsor.
    * Adds quickHtml - landing page for quick clock in or out

--- class/Controller/User/Punch.php
+++ class/Controller/User/Punch.php
@@ -9,14 +9,22 @@ namespace volunteer\Controller\User;
 
 use volunteer\Controller\SubController;
 use volunteer\View\KioskView;
+use volunteer\View\PunchView;
+use volunteer\View\SponsorView;
 use volunteer\Factory\VolunteerFactory;
 use volunteer\Factory\PunchFactory;
 use volunteer\Factory\SponsorFactory;
 use volunteer\Factory\ReasonFactory;
+use volunteer\Factory\SettingsFactory;
 use Canopy\Request;
 
 class Punch extends SubController
 {
+
+    protected function afterHtml(Request $request)
+    {
+        return PunchView::afterQuickPunch($request->pullGetString('hash'), $request->pullGetString('searchName'));
+    }
 
     protected function kioskHtml(Request $request)
     {
@@ -29,6 +37,73 @@ class Punch extends SubController
     protected function logInHtml(Request $request)
     {
         return \volunteer\View\VolunteerView::logInPrompt();
+    }
+
+    protected function quickPost(Request $request)
+    {
+        $volunteer = VolunteerFactory::getByHash($request->pullPostString('hash'));
+        if (empty($volunteer)) {
+            return PunchView::noMatchingHash($sponsor);
+        }
+        $currentPunch = PunchFactory::currentPunch($volunteer);
+
+        if ($currentPunch) {
+            $sponsorId = $currentPunch->sponsorId;
+            $sponsor = SponsorFactory::build($sponsorId);
+            PunchFactory::out($currentPunch);
+        } else {
+            $sponsorId = $request->pullPostInteger('sponsorId', true);
+            if (empty($sponsorId)) {
+                return SponsorView::badSponsorError();
+            }
+            $sponsor = SponsorFactory::build($sponsorId);
+            if (!$sponsor->quickLog) {
+                return PunchView::noQuickLog($sponsor);
+            }
+            $reasonId = $request->pullPostInteger('reasonId', true);
+
+            PunchFactory::in($volunteer, $sponsorId, $reasonId);
+        }
+        \Canopy\Server::forward("./volunteer/User/Punch/after?searchName={$sponsor->searchName}&hash={$volunteer->hash}");
+    }
+
+    protected function quickHtml(Request $request)
+    {
+        $hash = $request->pullGetString('hash');
+        /**
+         * @var array
+         */
+        /**
+         * @var Resource/Volunteer
+         */
+        $sponsor = $GLOBALS['currentSponsor'];
+        $volunteer = VolunteerFactory::getByHash($hash);
+        /**
+         * If volunteer hash doesn't match, send message.
+         */
+        if (empty($volunteer)) {
+            return PunchView::noMatchingHash($sponsor);
+        }
+
+        $preApproved = SettingsFactory::get('quickLogPreApproved');
+
+        /**
+         * If quick log not enabled, send message.
+         */
+        if (!$sponsor['quickLog']) {
+            return PunchView::noQuickLog($sponsor);
+        }
+
+        /**
+         * Get the current punch. If it is using the current sponsor, clock them
+         * out.
+         */
+        $punch = PunchFactory::currentPunch($volunteer);
+        if ($punch && (int) $punch->sponsorId === (int) $sponsor['id']) {
+            return PunchView::quickPunchOut($hash, $volunteer, $punch);
+        } else {
+            return PunchView::quickPunchIn($hash, $volunteer, $sponsor['id']);
+        }
     }
 
     protected function clockInReasonPost(Request $request)
